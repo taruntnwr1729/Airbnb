a092b46f6d7bcba25871bef6a17b3084288ee337
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-06-03 02:56:16 +0530

    Add selectable destination search options UI
    Introduce DestinationSearchOptions and selectedOption state to toggle between location, dates, and guests panels. Convert static rows into conditional views that expand when selected, add onTapGesture with animation to switch options, and adjust frame heights for expanded vs collapsed states. Extract common collapsed layout into a new ExtractedView component and refine placeholder content for dates and guests. Also include a Preview update and minor layout/styling tweaks. I felt this was complex a little to make with basic swift knowledge because of the animation

--- Airbnb/Core/Explore/DestinationSearchView.swift
+++ Airbnb/Core/Explore/DestinationSearchView.swift
@@ -7,9 +7,19 @@
 
 import SwiftUI
 
+enum DestinationSearchOptions{
+    case location
+    case dates
+    case guests
+    case nothing
+}
+
+
 struct DestinationSearchView: View {
     @Binding var show: Bool
     @State private var destination = ""
+    @State private var selectedOption: DestinationSearchOptions = .nothing
+    
     var body: some View {
         VStack{
             Button{
@@ -22,77 +32,160 @@ struct DestinationSearchView: View {
                     .foregroundStyle(.black)
             }
             VStack(alignment: .leading){
-                Text("Where to?")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                HStack{
-                    Image(systemName: "magnifyingglass")
-                        .imageScale(.small)
-                    TextField("Search Destination", text: $destination)
-                        .font(.subheadline)
-                }
-                .frame(height: 44)
-                .padding(.horizontal)
-                .overlay{
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(lineWidth: 1)
+                if (selectedOption == .location){
+                    Text("Where to?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    HStack{
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        TextField("Search Destination", text: $destination)
+                            .font(.subheadline)
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal)
+                    .overlay{
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 1)
+                        
+                            .foregroundStyle(Color(.systemGray4))
+                    }
                     
-                        .foregroundStyle(Color(.systemGray4))
+                } else{
+                    ExtractedView(title: "Where", description: "Add destination")
                 }
+                
             }
             .padding()
+            .frame(height: selectedOption == .location ? 120 : 64)
             .background(.white)
             .clipShape(RoundedRectangle(cornerRadius: 12))
             .padding()
             .shadow(radius: 10)
-            
-            
-            //Date Selection View
-            VStack{
-                HStack{
-                    Text("When")
-                        .foregroundStyle(.gray)
-                    Spacer()
-                    Text("Add dates")
-                        .fontWeight(.semibold)
-                        .font(.subheadline)
-                    
+            .onTapGesture {
+                withAnimation(.snappy){
+                    selectedOption = .location
                 }
-                
-                .padding()
+            }
+            
+            VStack(alignment: .leading, spacing: 5){
+                if (selectedOption == .dates) {
+                    Text("When's your trip?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    HStack{
+                        Text("From")
+                            .font(.subheadline)
+                            .foregroundStyle(.gray)
+                        Spacer()
+                        
+                        Text("Oct 6, 2026")
+                            
+                            
+                    }
+                    Divider()
+                    
+                    HStack{
+                        Text("To")
+                            .font(.subheadline)
+                            .foregroundStyle(.gray)
+                        
+                        Spacer()
+                        Text("Oct 10, 2026")
+                            
+                            
+                    }
+                    
+                    
+                }else {
+                    
+                    ExtractedView(title: "When", description: "Add dates")
+                        
+                        }
+                    
+                }.padding()
+                .frame(height: selectedOption == .dates ? 120 : 64)
                 .background(.white)
                 .clipShape(RoundedRectangle(cornerRadius: 12))
                 .padding()
                 .shadow(radius: 10)
-                
+            
+                .onTapGesture {
+                    withAnimation(.snappy){
+                        selectedOption = .dates
+                    }
             }
-            //num guests view
-            VStack{
-                HStack{
-                    Text("When")
-                        .foregroundStyle(.gray)
+            
+            
+            VStack(alignment: .leading, spacing: 5){
+                if (selectedOption == .guests) {
+                        Text("Who's coming?")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                        Spacer()
+                        HStack{
+                            Text("Adults")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                            Spacer()
+                        
+                    }
                     Spacer()
-                    Text("Add guests")
-                        .fontWeight(.semibold)
-                        .font(.subheadline)
                     
-                }
-                
-                .padding()
+                    
+                    
+                }else {
+                    
+                    ExtractedView(title: "Who", description: "Add guests")
+                        
+                        }
+                    
+                }.padding()
+                .frame(height: selectedOption == .guests ? 120 : 64)
                 .background(.white)
                 .clipShape(RoundedRectangle(cornerRadius: 12))
                 .padding()
                 .shadow(radius: 10)
-                
-                
-                
-            }
+                .onTapGesture {
+                    withAnimation(.snappy){
+                        selectedOption = .guests
+                    }
+                }
             
+            
+           
+            
+               
+                
         }
         
     }
 }
-
+    
 #Preview {
-    DestinationSearchView(show: .constant(false))
-}
+        DestinationSearchView(show: .constant(false))
+    }
+    
+
+    
+struct ExtractedView: View {
+        // 1. Define the properties (Swift automatically creates an initializer for these)
+    let title: String
+    let description: String
+        
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .foregroundStyle(.gray)
+                    Spacer()
+                    Text(description)
+                        .fontWeight(.semibold)
+                        .font(.subheadline)
+                }
+                
+            }
+        }
+    }
+    
+
