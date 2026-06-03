1a9c60cfe115a2f976d1fe2fe117a759e8656236
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-06-03 21:24:09 +0530

    Add Listing model and move Listing views
    Introduce a new Listing model (Identifiable & Codable) with properties for owner, location, capacity, pricing, rating, features, amenities and type. Add enums ListingFeatures, ListingAmenities and ListingType with presentation metadata (imageName, title, subtitle/id) and minor typo fixes. Also relocate ListingDetailView.swift and ListingItemView.swift into a new View subfolder to better organize listing-related views.

---
+++ Airbnb/Core/Listings/Model/Listing.swift
@@ -0,0 +1,122 @@
+//
+//  Listing.swift
+//  Airbnb
+//
+//  Created by Tarun Tanwar on 03/06/26.
+//
+
+import Foundation
+
+struct Listing: Identifiable, Codable {
+    let id: String
+    let ownerUid: String
+    let ownerName: String // Fixed typo: ownnerName -> ownerName
+    let ownerImageUrl: String
+    let numberOfBedrooms: Int
+    let numberOfBathroom: Int
+    let numberOfGuests: Int
+    let numberOfBeds: Int
+    var pricePerNight: Int
+    let latitude: Double
+    let longitude: Double
+    let address: String
+    let city: String
+    let state: String
+    let title: String
+    var rating: Double
+    var features: [ListingFeatures]
+    var amenities: [ListingAmenities]
+    let type: ListingType
+}
+
+enum ListingFeatures: Int, Codable, Identifiable, Hashable {
+    case selfCheckIn
+    case superHost
+    
+    var imageName: String {
+        switch self {
+        case .selfCheckIn: return "door.left.hand.open"
+        case .superHost: return "medal"
+        }
+    }
+
+    var title: String {
+        switch self {
+        case .selfCheckIn: return "Self check-in"
+        case .superHost: return "Superhost"
+        }
+    }
+
+    var subtitle: String {
+        switch self {
+        case .selfCheckIn:
+            return "Check yourself in with the keypad."
+        case .superHost:
+            return "Superhosts are experienced, highly rated hosts who are commited to providing greate stars for guests."
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
+
+enum ListingAmenities: Int, Codable, Identifiable, Hashable {
+    case pool
+    case kitchen
+    case wifi
+    case laundry
+    case tv
+    case balcony
+    case office
+    case alarmSystem
+    
+    // Moved inside the enum block where they belong:
+    var id: Int { return self.rawValue } // Fixed: Added missing explicit ': Int' type
+    
+    var title: String {
+        switch self {
+        case .pool: return "Pool"
+        case .kitchen: return "Kitchen"
+        case .wifi: return "Wifi"
+        case .laundry: return "Laundry"
+        case .tv: return "TV"
+        case .alarmSystem: return "Alarm System"
+        case .office: return "Office"
+        case .balcony: return "Balcony"
+        }
+    }
+
+    var imageName: String {
+        switch self {
+        case .pool: return "figure.pool.swim"
+        case .kitchen: return "fork.knife"
+        case .wifi: return "wifi"
+        case .laundry: return "washer"
+        case .tv: return "tv"
+        case .alarmSystem: return "checkerboard.shield"
+        case .office: return "pencil.and.ruler.fill"
+        case .balcony: return "building"
+        }
+        
+    }
+    
+
+    
+}
+
+enum ListingType: Int, Codable, Identifiable, Hashable {
+    case apartment
+    case house
+    case townHouse
+    case villa
+    
+    var description: String {
+        switch self {
+        case .apartment: return "Apartment"
+        case .house: return "House"
+        case .townHouse: return "Town Home"
+        case .villa: return "Villa"
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
